5b98704962afe76f907e1c8ada4d324e167dd2b8
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2019-05-02 00:19:38 +0700

    update #1

--- resources/views/dashBoard.blade.php
+++ resources/views/dashBoard.blade.php
@@ -1,107 +1,56 @@
-<!DOCTYPE html>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<html>
-<head>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+@extends('layouts.main')
 
-	<link rel="stylesheet" type="text/css" href="css/app.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-	<style>
-	#grad4 {
-		background-color: #212121;
-		background-position: relative;
-		background-repeat: no-repeat;
-		background-size: 100% 100% ;
-	}
-	.input-field input[type=text] + label, .materialize-textarea:focus:not([readonly]) + label {
-		color: #1a237e !important;
-	}
-
-	.input-field input[type=text], .materialize-textarea:focus:not([readonly]) {
-		border-bottom: 1px solid #1a237e !important;
-		box-shadow: 0 1px 0 0 #1a237e !important;
-	}
-
-
-	.input-field input[type=text]:focus + label, .materialize-textarea:focus:not([readonly]) + label {
-		color: #EEEEEE !important;
-	}
-
-	.input-field input[type=text]:focus, .materialize-textarea:focus:not([readonly]) {
-		border-bottom: 1px solid #9E9E9E !important;
-		box-shadow: 0 1px 0 0 #9E9E9E !important;
-	}
-</style>
-<title>H D M</title>
-</head>
-<body id="grad4" style="color: #ffffff;text-align: center;">
-	<!-- PART1 -->
-	<div class="col s12" style="width: 700px;margin: 0 auto;position: relative;" >
-		<div class="card blue-grey darken-2" style="display: block;overflow: auto;">
-			<div class="card-image waves-effect waves-block waves-light">
-				<img class="img1" src="https://i.imgur.com/kLSiSR6.jpg">
-				<!-- <img class="img1" src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t1.0-9/55529906_2216158175074102_8978448254035296256_o.jpg?_nc_cat=106&_nc_oc=AQnJf9q5yA-SXZXHgtQjgCrHJqW1iMsPrqKqYtVz4TcI-JbWneNJu3A1H1-ttsHw17s&_nc_ht=scontent.fsgn5-6.fna&oh=ab8f72fb57cdda1a6dc58d179deb6439&oe=5D38FDF1"> -->
-			</div>
-			<div class="card-title card-content" style="text-align: center;">
-				<a class="txt-grad2" style="letter-spacing: 0px;color: #ffffff;font-size: 25pt" href="#">HDM Confessions</a>
-				<hr>
-				<h6 style="text-align: left;color: #BBDEFB">- đây ↓ là chân dung người đang duyệt cfs nè :v -</h6>
-				<div class="row" style="text-align: left;padding: 10px;">
-					<img class="" src="{{$avatar}}" style="border: #CFD8DC solid 3px;border-radius: 50px;float: left;" width="70px" height="70px">
-					<div style="padding: 2px;padding-left: 10px">
-						<h4 style="">{{$user['first_name'].' '.$user['last_name']}}</h4>
-						<h5 style="color: #C8E6C9">{{$user['email']}}</h5>
-						<div style="padding: 5px">
-							<a href="/logout" class="waves-effect waves-light btn blue-grey darken-4"><b class="">đăng xuất</b></a>
-						</div>
-					</div>
-				</div>
-				<div class="col s12">
-						@foreach (['danger', 'warning', 'success', 'info'] as $msg)
-						@if(Session::has('alert-' . $msg))
-						<p class="alert alert-{{ $msg }}" style="font-size: 12pt"><b>{{ Session::get('alert-' . $msg) }}</b><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
-						@endif
-						@endforeach
-					</div>
-			</div>
-			<hr>
-			<h6><b>CÁC BẠN VUI LÒNG DUYỆT TỪ TRÊN XUỐNG ( ↓ ) NHÉ !</b></h6>
-			<div style="padding: 10px;text-align: left">
-				@foreach($data as $key => $data)
-				<form class="col s6" action="/page/id={{$data->id}}" method="get" style="padding: 10px">
-					<div style="width: 100%;padding: 10px;border: #e0e0e0 dashed 2px">
-						<div class="row" style="width: 100%;padding: 10px">
-							<div class="col s12 m6" style="float: left">
-								<h5 class="txt-grad2"><b>Chưa được duyệt !</b></h5><br>
-								<h6><b>{{$data->date}}</b></h6>
-								<h6><b>User: ẩn danh</b></h6>
-							</div>
-							<div class="col s12 m6" style="float: right;text-align: right;">
-								<div style="padding: 5px"><button type=submit class="waves-effect waves-light btn blue-grey darken-4"><b class="txt-grad2">Duyệt cfs này</b></button></div>
-								<div style="padding: 5px"><button type=submit class="waves-effect waves-light btn deep-orange darken-4"><b >Loại bỏ</b></button></div>
-							</div><br>
-						</div>
-						<div class="card grey darken-4" style="display: block;overflow: auto;padding: 10px">
-							<tr>    
-								<th>{{$data->noidung}}</th><br>              
-							</tr>
-						</div>
-					</div>
-				</form>
-				<hr>
-				@endforeach
+@section('mainpage-load')
+<div style="overflow-x: hidden">
+<hr>
+<div style="padding-left: 40px">
+	<h6 style="text-align: left;color: #BBDEFB">- đây ↓ là chân dung người đang duyệt cfs nè :v -</h6>
+	<div class="row" style="text-align: left;padding: 10px;">
+		<img class="" src="{{$avatar}}" style="border: #CFD8DC solid 3px;border-radius: 50px;float: left;" width="70px" height="70px">
+		<div style="padding: 2px;padding-left: 10px">
+			<h4 style="">{{$user['first_name'].' '.$user['last_name']}}</h4>
+			<h5 style="color: #C8E6C9">{{$user['email']}}</h5>
+			<div style="padding: 5px">
+				<a href="/logout" class="waves-effect waves-light btn blue-grey darken-4"><b class="txt-grad2">đăng xuất</b></a>
 			</div>
 		</div>
 	</div>
-</body>
-</html>
-<script> var $ = require('jquery');</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script type="text/javascript">
-	$(document).ready(function(){
-		$('select').formSelect();
-		$('input#input_text, textarea#textarea2').characterCounter();
-	});
-</script>
+</div>
+<div class="row" style="padding: 10px">
+	<div class="col-sm-12">
+		@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+		@if(Session::has('alert-' . $msg))
+		<p class="alert alert-{{ $msg }}" style="font-size: 12pt"><b>{{ Session::get('alert-' . $msg) }}</b><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a></p>
+		@endif
+		@endforeach
+	</div>
+</div>
+<hr>
+<h6><b>CÁC BẠN VUI LÒNG DUYỆT TỪ TRÊN XUỐNG ( ↓ ) NHÉ !</b></h6>
+<div style="padding: 10px;text-align: left">
+	@foreach($data as $key => $data)
+	<form class="col-sm-12" action="/page/id={{$data->id}}" method="get" style="padding: 10px">
+		<div style="width: 100%;padding: 10px;border: #e0e0e0 dashed 2px">
+			<div class="row" style="width: 100%;padding: 5px">
+				<div class="col-sm-6" style="float: left">
+					<h5 class="txt-grad2"><b>Chưa được duyệt !</b></h5><br>
+					<h6><b>{{$data->date}}</b></h6>
+					<h6><b>User: ẩn danh</b></h6>
+				</div>
+				<div class="col-sm-6" style="padding-right:0px;float: right;text-align: right;border-right: #99ccff dotted 12px">
+					<div style="padding: 5px"><button type=submit class="waves-effect waves-light btn blue-grey darken-4"><i style="font-size:15px" class="fas fa-check txt-grad1"></i><b class="txt-grad2"> Duyệt cfs này</b></button></div>
+					<div style="padding: 5px"><button type=submit class="waves-effect waves-light btn" style="background-color: #FF9800"><i style="font-size:15px" class="fas fa-trash-alt" style="color: black"></i><b style="color: black"> Loại bỏ</b></button></div>
+				</div><br>
+			</div>
+			<div class="card grey darken-4" style="display: block;overflow: auto;padding: 10px">
+				<tr>    
+					<th>{{$data->noidung}}</th><br>              
+				</tr>
+			</div>
+		</div>
+	</form>
+	<hr>
+	@endforeach
+</div>
+</div>
+@stop
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,141 @@
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+	#grad4 {
+		background-color: #212121;
+		background-position: relative;
+		background-repeat: no-repeat;
+		background-size: 100% 100% ;
+	}
+	.input-field input[type=text] + label, .materialize-textarea:focus:not([readonly]) + label {
+		color: #1a237e !important;
+	}
+
+	.input-field input[type=text], .materialize-textarea:focus:not([readonly]) {
+		border-bottom: 1px solid #1a237e !important;
+		box-shadow: 0 1px 0 0 #1a237e !important;
+	}
+
+
+	.input-field input[type=text]:focus + label, .materialize-textarea:focus:not([readonly]) + label {
+		color: #EEEEEE !important;
+	}
+
+	.input-field input[type=text]:focus, .materialize-textarea:focus:not([readonly]) {
+		border-bottom: 1px solid #9E9E9E !important;
+		box-shadow: 0 1px 0 0 #9E9E9E !important;
+	}
+</style>
+<title>HDM Confessions</title>
+</head>
+<body id="grad4" style="color: #ffffff;text-align: center;">
+	<!-- PART1 -->
+	<div class="col-sm-12">
+		<div class="row">
+			<div class="col-xl-3 col-lg-1">
+			</div>
+			<div class="col-xl-1 col-lg-2 col-sm-12" style="padding: 15px">
+				<div style="padding:10px">
+					<div class="row desktopPanel" style="">
+						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
+							<a href="/" class="waves-effect waves-block waves-light" style="width:100px;height: 100px;padding:10px;border: #7bc6cc dashed 3px;float: right;color:#ffffff;justify-content: center;align-items: center;text-decoration: none;background-color: #263238">
+								<i class="fab fa-fort-awesome txt-grad2" style="font-size: 32pt;"></i><br>
+								<h6 style="padding-top:10px">Trang chủ</h6>
+							</a>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
+							<a href="#" class="waves-effect waves-block waves-light" style="width:100px;height: 100px;padding:10px;border: #7bc6cc dashed 3px;float: right;color:#ffffff;justify-content: center;align-items: center;text-decoration: none;background-color: #37474F">
+								<i class="fab fa-facebook txt-grad2" style="font-size: 32pt;"></i><br>
+								<h6 style="padding-top:10px">Đọc cfs</h6>
+							</a>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
+							<a href="#" class="waves-effect waves-block waves-light" style="width:100px;height: 100px;padding:10px;border: #7bc6cc dashed 3px;float: right;color:#ffffff;justify-content: center;align-items: center;text-decoration: none;background-color: #37474F">
+								<i class="fas fa-feather-alt txt-grad2" style="font-size: 32pt;"></i><br>
+								<h6 style="padding-top:10px">Tâm sự :></h6>
+							</a>
+						</div>
+						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
+							<a href="/admin" class="waves-effect waves-block waves-light" style="width:100px;height: 100px;padding:10px;border: #7bc6cc dashed 3px;float: right;color:#ffffff;justify-content: center;align-items: center;text-decoration: none;background-color: #37474F">
+								<i class="fas fa-atom txt-grad2" style="font-size: 32pt;"></i><br>
+								<h6 style="padding-top:10px">ahjhi ?</h6>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-7 col-sm-10" style="width: 620px;margin: 0 auto;position: relative;" ><!-- blue-grey darken-2 -->
+				<div class="card blue-grey darken-2" style="display: block;overflow: auto;">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img class="img1" src="https://i.imgur.com/kLSiSR6.jpg">
+					</div>
+					<div class="card-title card-content" style="text-align: center;">
+						<a class="hdmmainText txt-grad2" style="letter-spacing: 0px;color: #ffffff;font-size: 30pt" href="#"><b>HDM Confessions</b></a><br>
+						<img src="/funny/heart2.gif" width="30px" height="30px">
+					<!-- <marquee scrollamount="5">
+						<h6>chào mừng các bạn đến với ngôi nhà mới của chúng tớ ^^</h6>
+					</marquee> -->
+
+					<div class="mobilePanel col-sm-12" style="display: none">
+						<hr>
+						<div class="row" style="padding: 5px;">
+							<a href="/" class="col-3 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #263238;display: flex;justify-content: center;align-items: center">
+								<i class="fab fa-fort-awesome txt-grad2" style="font-size: 15pt;"></i>
+							</a>
+							<a href="#" class="col-3 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #37474F;display: flex;justify-content: center;align-items: center">
+								<i class="fab fa-facebook txt-grad2" style="font-size: 15pt;"></i>
+							</a>
+							<a href="#" class="col-3 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #37474F;display: flex;justify-content: center;align-items: center">
+								<i class="fas fa-feather-alt txt-grad2" style="font-size: 15pt;"></i>
+							</a>
+							<a href="/admin" class="col-3 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #37474F;display: flex;justify-content: center;align-items: center">
+								<i class="fas fa-atom txt-grad2" style="font-size: 15pt;"></i>
+							</a>
+						</div>
+						<hr>
+					</div>
+				</div>
+				<div style="padding-bottom: 10px">
+					<img src="/funny/star2.gif" width="30px" height="30px">
+					<img src="/funny/star1.gif" width="30px" height="30px">
+					<img src="/funny/star2.gif" width="30px" height="30px">
+					<img src="/funny/star1.gif" width="30px" height="30px">
+					<img src="/funny/star2.gif" width="30px" height="30px">
+					<img src="/funny/star1.gif" width="30px" height="30px">
+					<img src="/funny/star2.gif" width="30px" height="30px">
+					<img src="/funny/star1.gif" width="30px" height="30px">
+					<img src="/funny/star2.gif" width="30px" height="30px">
+				</div>
+				@yield('mainpage-load')
+				<div style="text-align: center; padding: 30px;">
+					<p><i class="txt-grad2" style="font-size: 10pt">HDM Confessions 2019 - Hương Đầu Mùa</i></p>
+					<p><i class="txt-grad2" style="font-size: 10pt">THPT Nguyễn Huệ - Huế .</i></p>
+				</div>
+				<!-- insert -->
+			</div>
+		</div>
+		<div class="col-xl-4 col-lg-2" style="margin: 0 auto;position: relative;" >
+		</div>
+	</div>
+</div>
+</body>
+</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('select').formSelect();
+		$('textarea#textarea2').characterCounter();
+	});
+</script>
