7238577f8580947a113f375730b258a8266a4452
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2019-05-03 12:16:24 +0700

    fix frontend 12pm 3/5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -42,31 +42,31 @@
 
 <title>HDM Confessions</title>
 </head>
-<body id="grad4" style="color: #ffffff;text-align: center;">
+<body id="grad4" style="color: #ffffff;text-align: center;font-family: 'Nunito', sans-serif;">
 	<!-- PART1 -->
 	<div class="col-sm-12">
-		<div class="row">
-			<div class="col-xl-2 col-lg-1">
+		<div class="row" style="">
+			<div class="col-xl-3 col-lg-2">
 			</div>
-			<div class="col-xl-1 col-lg-2 col-sm-12" style="padding: 15px">
+			<div class="fixed-top right" style="padding: 15px;z-index: 1">
 				<div style="padding:10px">
 					<div class="row desktopPanel" style="">
 						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
 							<a href="{{ route('home') }}" class="waves-effect waves-block waves-light" style="width:100px;height: 100px;padding:10px;border: #7bc6cc dashed 3px;float: right;color:#ffffff;justify-content: center;align-items: center;text-decoration: none;background-color: #263238">
-								<i class="fab fa-fort-awesome txt-grad2" style="font-size: 32pt;"></i><br>
-								<h6 style="padding-top:10px">Trang chủ</h6>
+								<i class="fas fa-dice-d20 txt-grad2" style="font-size: 32pt;"></i><br>
+								<h6 style="padding-top:10px">Nhà</h6>
 							</a>
 						</div>
 						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
 							<a href="#" class="waves-effect waves-block waves-light" style="width:100px;height: 100px;padding:10px;border: #7bc6cc dashed 3px;float: right;color:#ffffff;justify-content: center;align-items: center;text-decoration: none;background-color: #37474F">
-								<i class="fab fa-facebook txt-grad2" style="font-size: 32pt;"></i><br>
+								<i class="fas fa-book txt-grad2" style="font-size: 32pt;"></i><br>
 								<h6 style="padding-top:10px">Đọc cfs</h6>
 							</a>
 						</div>
 						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
 							<a href="#" class="waves-effect waves-block waves-light" style="width:100px;height: 100px;padding:10px;border: #7bc6cc dashed 3px;float: right;color:#ffffff;justify-content: center;align-items: center;text-decoration: none;background-color: #37474F">
 								<i class="fas fa-feather-alt txt-grad2" style="font-size: 32pt;"></i><br>
-								<h6 style="padding-top:10px">Tâm sự :></h6>
+								<h6 style="padding-top:10px">HDM Blog</h6>
 							</a>
 						</div>
 						<div class="col-xl-12 col-lg-12 col-sm-3 col-3" style="padding: 5px;">
@@ -78,13 +78,13 @@
 					</div>
 				</div>
 			</div>
-			<div class="mainContent" style="width: 620px;margin: 0 auto;position: relative;" ><!-- blue-grey darken-2 -->
+			<div class="mainContent" style="z-index:99;width: 620px;margin: 0 auto;position: relative;" ><!-- blue-grey darken-2 -->
 				<div class="card blue-grey darken-2" style="display: block;overflow: auto;">
 					<div class="card-image waves-effect waves-block waves-light">
 						<img class="img1 activator" src="{{ URL::asset('sources/images/background.jpg') }}">
 					</div>
 					<div class="card-title card-content" style="text-align: center;">
-						<a class="hdmmainText txt-grad2" style="letter-spacing: 0px;color: #ffffff;font-size: 30pt" href="#"><b>HDM Confessions</b></a><br>
+						<a class="hdmmainText txt-grad2" style="letter-spacing: 0px;color: #ffffff;font-size: 30pt;font-family: 'Sofia', cursive;" href="#"><b>HDM Confessions</b></a><br>
 						<img src="/funny/heart2.gif" width="30px" height="30px">
 					<!-- <marquee scrollamount="5">
 						<h6>chào mừng các bạn đến với ngôi nhà mới của chúng tớ ^^</h6>
@@ -95,10 +95,10 @@
 						<h6><kbd>này là menu nè :3</kbd></h6>
 						<div class="row" style="padding: 5px;">
 							<a href="/" class="col-3 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #263238;display: flex;justify-content: center;align-items: center">
-								<i class="fab fa-fort-awesome txt-grad2" style="font-size: 15pt;"></i>
+								<i class="fas fa-dice-d20 txt-grad2" style="font-size: 15pt;"></i>
 							</a>
 							<a href="#" class="col-3 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #37474F;display: flex;justify-content: center;align-items: center">
-								<i class="fab fa-facebook txt-grad2" style="font-size: 15pt;"></i>
+								<i class="fas fa-book txt-grad2" style="font-size: 15pt;"></i>
 							</a>
 							<a href="#" class="col-3 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #37474F;display: flex;justify-content: center;align-items: center">
 								<i class="fas fa-feather-alt txt-grad2" style="font-size: 15pt;"></i>
@@ -109,17 +109,6 @@
 						</div>
 						<hr>
 					</div>
-				</div>
-				<div style="padding-bottom: 10px">
-					<img src="/funny/star2.gif" width="30px" height="30px">
-					<img src="/funny/star1.gif" width="30px" height="30px">
-					<img src="/funny/star2.gif" width="30px" height="30px">
-					<img src="/funny/star1.gif" width="30px" height="30px">
-					<img src="/funny/star2.gif" width="30px" height="30px">
-					<img src="/funny/star1.gif" width="30px" height="30px">
-					<img src="/funny/star2.gif" width="30px" height="30px">
-					<img src="/funny/star1.gif" width="30px" height="30px">
-					<img src="/funny/star2.gif" width="30px" height="30px">
 				</div>
 				@yield('mainpage-load')
 				<div style="text-align: center; padding: 30px;">
@@ -152,7 +141,7 @@
 				<!-- insert -->
 			</div>
 		</div>
-		<div class="col-xl-3 col-lg-2" style="margin: 0 auto;position: relative;" >
+		<div class="col-xl-1 col-lg-2" style="margin: 0 auto;position: relative;" >
 		</div>
 	</div>
 </div>
