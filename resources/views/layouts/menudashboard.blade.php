992b92398766930560a651a9b17d1e28dd085d4e
pnnguyen9999 <pnnguyen9999@outlook.com.vn>
2019-05-06 16:47:59 +0700

    refix xiu

--- resources/views/layouts/menudashboard.blade.php
+++ resources/views/layouts/menudashboard.blade.php
@@ -2,7 +2,7 @@
 	<hr>
 	<div class="row" style="padding: 5px;">
 		<a class="col-3"></a>
-		<a href="/admin?" class="col-2 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #FAFAFA;display: flex;justify-content: center;align-items: center">
+		<a href="/admin" class="col-2 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #FAFAFA;display: flex;justify-content: center;align-items: center">
 			<i class="fas fa-list-ol txt-grad1" style="font-size: 15pt;"></i>
 		</a>
 		<a href="/admin/recoverpage" class="col-2 waves-effect waves-block waves-light" style="width:35px;height: 35px;border: #7bc6cc dashed 1px;color:#ffffff;text-decoration: none;background-color: #FAFAFA;display: flex;justify-content: center;align-items: center">
