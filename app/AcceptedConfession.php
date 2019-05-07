4a1321c7900e3a835d8a197b6a864f48f3900fff
bquangDinh <buiquangdinh1751@gmail.com>
2019-05-07 22:27:12 +0700

    Fix the last one

--- app/AcceptedConfession.php
+++ app/AcceptedConfession.php
@@ -9,6 +9,10 @@ class AcceptedConfession extends Model
     protected $table = 'accepted_confession';
 
     public function confession(){
-      return $this->belongsTo('App\Confession');
+      return $this->belongsTo('App\Confession','confession_id');
+    }
+
+    public function user(){
+      return $this->belongsTo('App\User','accept_by');
     }
 }
--- app/DeletedConfession.php
+++ app/DeletedConfession.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class DeletedConfession extends Model
 {
     protected $table = 'deleted_confession';
+
+    public function confession(){
+      return $this->belongsTo('App\Confession','confession_id');
+    }
+
+    public function user(){
+      return $this->belongsTo('App\User','delete_by');
+    }
 }
